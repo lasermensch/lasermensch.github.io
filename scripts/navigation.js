eb4d74b8aa5505a8133448aa4ab890ecb0d7a395
Lasermensch <sten.erik.sundberg@gmail.com>
2022-04-06 16:19:41 +0200

    nav done

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -2,6 +2,9 @@ const main = document.querySelector('#main');
 
 document.querySelector('#linkToHero').addEventListener('click', function () {loadSection('hero')});
 document.querySelector('#linkToSkills').addEventListener('click', function() {loadSection('service')});
+document.querySelector('#linkToProjects').addEventListener('click', function() {loadSection('projects')});
+document.querySelector('#linkToAbout').addEventListener('click', function() {loadSection('about')});
+document.querySelector('#linkToContacts').addEventListener('click', function() {loadSection('contact')});
 
 async function loadSection (element) {
     let content = "";
