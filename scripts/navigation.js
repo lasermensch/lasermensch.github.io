57eec3eec122eba5034e4fe1933e9e7eeec8d9d7
Erik Sundberg <sten.erik.sundberg@gmail.com>
2022-04-07 12:23:33 +0200

    onload

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,5 @@
+window.onload = function () {console.log('tada!');loadSection('hero');}
+
 const main = document.querySelector('#main');
 
 document.querySelector('#linkToHero').addEventListener('click', function () {loadSection('hero')});
@@ -5,18 +7,20 @@ document.querySelector('#linkToSkills').addEventListener('click', function() {lo
 document.querySelector('#linkToProjects').addEventListener('click', function() {loadSection('projects')});
 document.querySelector('#linkToAbout').addEventListener('click', function() {loadSection('about')});
 document.querySelector('#linkToContacts').addEventListener('click', function() {loadSection('contact')});
+document.querySelector('#linkToHome').addEventListener('click', function() {loadSection('hero')});
 
 async function loadSection (element) {
     let content = "";
-    main.innerHTML = "";
+ 
     await fetch(`../${element}.html`)
         .then(response => response.text()).then(data => content = data);
   
-    console.log(content);
+
     
     main.innerHTML = content;
     
     
 }
 
-loadSection('hero');
+
+
